8e3bba45a9e7c842fd3f9e4ccae763cbf4ec9c3a
th_abir303 <bsse1321@iit.du.ac.bd>
2024-12-11 02:34:04 +0600

    Update Education info

--- Portfolio/app/resume/page.jsx
+++ Portfolio/app/resume/page.jsx
@@ -68,7 +68,7 @@ const experience = {
 const education = {
     icon : "/assets/resume/cap.svg",
     title : "Education",
-    description : "I am completing my Bachelor's Sciece in Software Enginerring from University of Dhaka.",
+    description : "I am completing my Bachelor of Science in Software Engineering from University of Dhaka.",
     items : [
         {
             institution : "University of Dhaka",
@@ -250,36 +250,37 @@ const Resume = () => {
                         </TabsContent>
 
                         <TabsContent value="Education" className="w-full">
-                            <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                                <h3 className="text-4xl font-bold">{education.title}</h3>
-                                <p className="max-w-[600px] mx-auto xl:mx-0 text-white/60">
-                                {education.description}
-                                </p>
-                                
-                                <ScrollArea className="h-[400px]">
-                                <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                                    {education.items.map((item, index) => (
-                                    <li
-                                        key={index}
-                                        className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col items-center justify-center lg:items-start gap-1"
-                                    >
-                                        <span className="text-accent">{item.duration}</span>
-                                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
-                                        {item.degree}
-                                        </h3>
+    <div className="flex flex-col gap-[30px] text-center xl:text-left">
+        <h3 className="text-4xl font-bold">{education.title}</h3>
+        <p className="max-w-[600px] mx-auto xl:mx-0 text-white/60">
+            {education.description}
+        </p>
 
-                                        <div className="flex items-center gap-3">
-                                        <span className="w-[6px] h-[6px] bg-accent rounded-full"></span>
-                                        <p className="text-white/60">{item.institution}</p>
-                                        </div>
-                                        
-                                        <p className="text-white/60">{item.result}</p> 
-                                    </li>
-                                    ))}
-                                </ul>
-                                </ScrollArea>
-                            </div>
-                        </TabsContent>
+        <ScrollArea className="h-[400px]">
+            <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                {education.items.map((item, index) => (
+                    <li
+                        key={index}
+                        className="bg-[#232329] py-6 px-10 rounded-xl flex flex-col items-center justify-center lg:items-start gap-3"
+                    >
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] text-center lg:text-left">
+                            {item.degree}
+                        </h3>
+
+                        <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] bg-accent rounded-full"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                        </div>
+
+                        <p className="text-white/60">{item.result}</p> 
+                    </li>
+                ))}
+            </ul>
+        </ScrollArea>
+    </div>
+</TabsContent>
+
 
                         <TabsContent value="Skills" className="w-full h-full">
                             <div className="flex flex-col gap-[30px]">
